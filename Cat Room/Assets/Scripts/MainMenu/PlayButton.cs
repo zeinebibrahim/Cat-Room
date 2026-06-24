5ca2c91c0119e600ea7d4c84aced6e4a6c799b97
zeinebibrahim <zeineb.ibrahim01@outlook.com>
2026-06-24 02:48:35 +0200

    Update: menu

--- Cat Room/Assets/Scripts/MainMenu/PlayButton.cs
+++ Cat Room/Assets/Scripts/MainMenu/PlayButton.cs
@@ -4,8 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class PlayButton : MonoBehaviour
 {
+    [SerializeField] GameObject tutorialCanvas;
     public void PlayGame()
     {
         SceneManager.LoadScene("LevelTwo");
+    }
+
+    public void TutorialButton()
+    {
+        tutorialCanvas.SetActive(true);
+    }
+
+    public void CloseTutorial()
+    {
+        tutorialCanvas.SetActive(false);
     }
 }
